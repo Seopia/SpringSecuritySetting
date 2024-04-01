02df934eef72bd2ba8072f83e59131ceaa86c75f
Seopia <wjdwltjq7289@gmail.com>
2024-04-01 21:56:01 +0900

    cors 설정

--- src/main/java/com/bu200/bu200/security/controller/MainController.java
+++ src/main/java/com/bu200/bu200/security/controller/MainController.java
@@ -2,11 +2,13 @@ package com.bu200.bu200.security.controller;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
+@CrossOrigin
 public class MainController {
 
     @GetMapping("/")
